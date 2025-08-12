569c8ad8d22f54f0197b5094d6e49d08bd29be2e
Michal Hanajik <michal@hanajik.com>
2025-08-12 09:57:02 +0200

    assetsfs inspired by bindata

--- cmd/asseter/main.go
+++ cmd/asseter/main.go
@@ -11,20 +11,12 @@ import (
 	"github.com/pudottapommin/asseter"
 )
 
-// PLAN
-// Step 1: clean output directory
-// Step 2: copy assets
-// Step 3: copy fonts if defined
-// Step 4: generate hash if needed
-// Step 5: generate assets_gen.go
-
 func main() {
 	if len(os.Args) < 2 {
 		fmt.Println("Usage: asseter <command> [options]")
 		fmt.Println("Commands:")
 		fmt.Println("  copy     Copy assets to output directory")
-		fmt.Println("  gen   Generate assets_gen.go file")
-		fmt.Println("  hash     Version assets files in directory")
+		fmt.Println("  gen   Generate bindata assets")
 		os.Exit(1)
 	}
 
@@ -46,7 +38,7 @@ func main() {
 		if err != nil {
 			log.Fatalln(err)
 		}
-		if err = cmd.Run(ctx); err != nil {
+		if err = cmd.Run(); err != nil {
 			log.Fatalln(err)
 		}
 		fmt.Println("Elapsed ", time.Since(t))
@@ -74,24 +66,42 @@ func newCopyHandlerFromCLI() (cmd *asseter.CopyHandler, err error) {
 	return
 }
 
-func newAssetsHandlerFromCLI() (cmd *asseter.AssetsHandler, err error) {
-	o := asseter.AssetsOptions{}
+// func newAssetsHandlerFromCLI() (cmd *asseter.AssetsHandler, err error) {
+// 	o := asseter.AssetsOptions{}
+// 	flagSet := flag.NewFlagSet("gen", flag.ExitOnError)
+// 	flagSet.StringVar(&o.Cwd, "cwd", "", "Working directory ( where are node_modules )")
+// 	flagSet.StringVar(&o.SrcDir, "src", "dist", "Directory for assets")
+// 	flagSet.StringVar(&o.DistDir, "dist", "assets", "Directory where assets will be generated")
+// 	flagSet.StringVar(&o.WebDir, "web", "", "Serve assets from web directory")
+// 	flagSet.StringVar(&o.Pkg, "pkg", "assets", "Package name for generated file")
+// 	flagSet.StringVar(&o.Server, "server", "http", "Binding for HTTP server (http,gin)")
+// 	flagSet.StringVar(&o.UrlPrefix, "urlPrefix", "/static", "URL path to prepend to all assets")
+// 	flagSet.BoolVar(&o.IsEmbed, "embed", false, "Flag to embed assets into binary")
+// 	flagSet.BoolVar(&o.ShouldHash, "hash", false, "Flag to hash assets files")
+// 	flagSet.Var(&o.Exclude, "exclude", "Exclude paths by glob")
+// 	if err = flagSet.Parse(os.Args[2:]); err != nil {
+// 		return nil, err
+// 	}
+//
+// 	cmd, err = asseter.NewAssetsHandler(o)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	return
+// }
+
+func newAssetsHandlerFromCLI() (cmd *asseter.AssetsFsHandler, err error) {
+	o := asseter.AssetsFsOptions{}
 	flagSet := flag.NewFlagSet("gen", flag.ExitOnError)
 	flagSet.StringVar(&o.Cwd, "cwd", "", "Working directory ( where are node_modules )")
-	flagSet.StringVar(&o.SrcDir, "src", "dist", "Directory for assets")
-	flagSet.StringVar(&o.DistDir, "dist", "assets", "Directory where assets will be generated")
-	flagSet.StringVar(&o.WebDir, "web", "", "Serve assets from web directory")
+	flagSet.StringVar(&o.Src, "src", "assets", "Directory for assets")
+	flagSet.StringVar(&o.Out, "out", "bindata.asseter.go", "Output filename")
 	flagSet.StringVar(&o.Pkg, "pkg", "assets", "Package name for generated file")
-	flagSet.StringVar(&o.Server, "server", "http", "Binding for HTTP server (http,gin)")
-	flagSet.StringVar(&o.UrlPrefix, "urlPrefix", "/static", "URL path to prepend to all assets")
-	flagSet.BoolVar(&o.IsEmbed, "embed", false, "Flag to embed assets into binary")
-	flagSet.BoolVar(&o.ShouldHash, "hash", false, "Flag to hash assets files")
-	flagSet.Var(&o.Exclude, "exclude", "Exclude paths by glob")
 	if err = flagSet.Parse(os.Args[2:]); err != nil {
 		return nil, err
 	}
 
-	cmd, err = asseter.NewAssetsHandler(o)
+	cmd, err = asseter.NewAssetsFsHandler(o)
 	if err != nil {
 		return nil, err
 	}
--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/pudottapommin/asseter
 
 go 1.24.6
 
-require golang.org/x/sync v0.16.0
+require (
+	github.com/klauspost/compress v1.18.0
+	github.com/valyala/bytebufferpool v1.0.0
+	golang.org/x/sync v0.16.0
+)
